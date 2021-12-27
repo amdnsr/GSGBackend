66fad38e702e3be2ccafd15ce33d197387a294b5
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2021-12-27 22:35:58 +0530

    add models for login and userprofile

--- app/models/request_response_models.py
+++ app/models/request_response_models.py
@@ -1,4 +1,6 @@
-from typing import List
+from datetime import datetime
+
+from typing import List, Optional
 from pydantic import BaseModel
 
 
@@ -32,3 +34,41 @@ class CreateAccountRequest(BaseModel):
     phone_number: str
     password: str
     confirm_password: str
+
+
+class CreateAccountResponse(BaseModel):
+    pass
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+
+class Token(BaseModel):
+    token_type: str
+    access_token: str
+    expire_at: datetime
+    refresh_token: str
+    refresh_expire_at: datetime
+
+
+class TokenPayload(BaseModel):
+    exp: datetime
+    sub: Optional[str] = None
+    refresh: Optional[bool] = None
+
+
+class TokenRefresh(BaseModel):
+    refresh_token: str
+
+
+class LoginResponse(Token):
+    pass
+
+
+class UserProfileResponse(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
