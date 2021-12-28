ccbddbe6052fdf6b52deb9ca0e75d781aaab4ba7
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2021-12-29 02:27:42 +0530

    implement past 2 commits and print() method

--- app/config/configurations.py
+++ app/config/configurations.py
@@ -1,7 +1,7 @@
 import json
 import os
 from typing import List
-from app.utils.helpers import SingleInstanceMetaClass
+from app.utils.helpers import SingleInstanceMetaClass, get_env_variable
 
 
 class DBConfig(metaclass=SingleInstanceMetaClass):
@@ -35,25 +35,44 @@ class AppConfig(metaclass=SingleInstanceMetaClass):
     app_root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    title: str
-    version: str
-    description: str
-    host: str
-    port: int
-    reload: bool
-    debug: str
+    TITLE: str
+    VERSION: str
+    DESCRIPTION: str
+    HOST: str
+    PORT: int
+    RELOAD: bool
+    DEBUG: str
 
     def load_configurations(self, config_path='config.json'):
         config_json_path = os.path.join(DBConfig.current_dir, config_path)
         config_json = json.loads(open(config_json_path, 'r').read())
         
-        AppConfig.title = config_json["app_details"]["title"]
-        AppConfig.version = config_json["app_details"]["version"]
-        AppConfig.description = config_json["app_details"]["description"]
-        AppConfig.host = config_json["app_details"]["host"]
-        AppConfig.port = config_json["app_details"]["port"]
-        AppConfig.reload = config_json["app_details"]["reload"]
-        AppConfig.debug = config_json["app_details"]["debug"]
+        json_dict = config_json["app_details"]
+        
+        AppConfig.TITLE = get_env_variable("TITLE", json_dict, str)
+        AppConfig.VERSION = get_env_variable("VERSION", json_dict, str)
+        AppConfig.DESCRIPTION = get_env_variable("DESCRIPTION", json_dict, str)
+        AppConfig.HOST = get_env_variable("HOST", json_dict, str)
+        AppConfig.PORT = get_env_variable("PORT", json_dict, int)
+        AppConfig.RELOAD = get_env_variable("RELOAD", json_dict, bool)
+        AppConfig.DEBUG = get_env_variable("DEBUG", json_dict, bool)
+
+    @classmethod
+    def print(self):
+        app_config = \
+        f"""
+        AppConfig
+        {"-"*50}
+        TITLE: {AppConfig.TITLE}
+        VERSION: {AppConfig.VERSION}
+        DESCRIPTION: {AppConfig.DESCRIPTION}
+        HOST: {AppConfig.HOST}
+        PORT: {AppConfig.PORT}
+        RELOAD: {AppConfig.RELOAD}
+        DEBUG: {AppConfig.DEBUG}
+        {"-"*50}
+        """
+        return app_config
 
 
 config_path = 'config.json'
--- app/settings/app_settings.py
+++ app/settings/app_settings.py
@@ -1,14 +1,14 @@
 import json
 import os
 from pydantic import AnyHttpUrl, AnyUrl, BaseSettings, EmailStr, validator
-from typing import List, Set, Union
+from typing import List, Set, Union, get_type_hints
 # Literal from typing_extensions for python 3.7 support, remove if not needed
 try:
     from typing import Literal
 except ImportError:
     from typing_extensions import Literal
 #
-from app.utils.helpers import SingleInstanceMetaClass
+from app.utils.helpers import SingleInstanceMetaClass, get_env_variable
 
 
 class Settings(metaclass=SingleInstanceMetaClass):
@@ -24,47 +24,34 @@ class Settings(metaclass=SingleInstanceMetaClass):
     BACKEND_CORS_ORIGINS: Union[str, List[AnyHttpUrl]]
 
     def load_settings(self, settings_path=None):
-        if not settings_path:
-            Settings.ALGORITHM = os.getenv("ALGORITHM", None)
-            Settings.SECRET_KEY = os.getenv("SECRET_KEY", None)
-            Settings.ENVIRONMENT = os.getenv("ENVIRONMENT")
-            Settings.ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", None)
-            Settings.REFRESH_TOKEN_EXPIRE_MINUTES = os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", None)
-            Settings.BACKEND_CORS_ORIGINS = os.getenv("BACKEND_CORS_ORIGINS", None)
-            return
+        if settings_path:
+            settings_json_path = os.path.join(Settings.current_dir, settings_path)
+            settings_json = json.loads(open(settings_json_path, 'r').read())
+        else:
+            settings_json = dict()
         
-        settings_json_path = os.path.join(Settings.current_dir, settings_path)
-        settings_json = json.loads(open(settings_json_path, 'r').read())
-
-        if settings_json["ALGORITHM"] is None:
-            Settings.ALGORITHM = os.getenv("ALGORITHM", None)
-        else:
-            Settings.ALGORITHM = settings_json["ALGORITHM"]
-
-        if settings_json["SECRET_KEY"] is None:
-            Settings.SECRET_KEY = os.getenv("SECRET_KEY", None)
-        else:
-            Settings.SECRET_KEY = settings_json["SECRET_KEY"]
-
-        if settings_json["ENVIRONMENT"] is None:
-            Settings.ENVIRONMENT = os.getenv("ENVIRONMENT")
-        else:
-            Settings.ENVIRONMENT = settings_json["ENVIRONMENT"]
-
-        if settings_json["ACCESS_TOKEN_EXPIRE_MINUTES"] is None:
-            Settings.ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", None)
-        else:
-            Settings.ACCESS_TOKEN_EXPIRE_MINUTES = settings_json["ACCESS_TOKEN_EXPIRE_MINUTES"]
-
-        if settings_json["REFRESH_TOKEN_EXPIRE_MINUTES"] is None:
-            Settings.REFRESH_TOKEN_EXPIRE_MINUTES = os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", None)
-        else:
-            Settings.REFRESH_TOKEN_EXPIRE_MINUTES = settings_json["REFRESH_TOKEN_EXPIRE_MINUTES"]
-
-        if settings_json["BACKEND_CORS_ORIGINS"] is None:
-            Settings.BACKEND_CORS_ORIGINS = os.getenv("BACKEND_CORS_ORIGINS", None)
-        else:
-            Settings.BACKEND_CORS_ORIGINS = settings_json["BACKEND_CORS_ORIGINS"]
+        Settings.ALGORITHM = get_env_variable("ALGORITHM", settings_json, str)
+        Settings.SECRET_KEY = get_env_variable("SECRET_KEY", settings_json, str)
+        Settings.ENVIRONMENT = get_env_variable("ENVIRONMENT", settings_json, str)
+        Settings.ACCESS_TOKEN_EXPIRE_MINUTES = get_env_variable("ACCESS_TOKEN_EXPIRE_MINUTES", settings_json, str)
+        Settings.REFRESH_TOKEN_EXPIRE_MINUTES = get_env_variable("REFRESH_TOKEN_EXPIRE_MINUTES", settings_json, str)
+        Settings.BACKEND_CORS_ORIGINS = get_env_variable("BACKEND_CORS_ORIGINS", settings_json, str)
+    
+    @classmethod
+    def print(self):
+        settings = \
+        f"""
+        Settings
+        {"-"*50}
+        ALGORITHM: {Settings.ALGORITHM}
+        SECRET_KEY: {Settings.SECRET_KEY}
+        ENVIRONMENT: {Settings.ENVIRONMENT}
+        ACCESS_TOKEN_EXPIRE_MINUTES: {Settings.ACCESS_TOKEN_EXPIRE_MINUTES}
+        REFRESH_TOKEN_EXPIRE_MINUTES: {Settings.REFRESH_TOKEN_EXPIRE_MINUTES}
+        BACKEND_CORS_ORIGINS: {Settings.BACKEND_CORS_ORIGINS}
+        {"-"*50}
+        """
+        return settings
     
     @classmethod
     def get_cors_list(self):
