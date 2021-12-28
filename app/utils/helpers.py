d56cf4376c764fbd50631a358921cfc29310f228
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2021-12-29 02:22:52 +0530

    add ability to get env variables from env or json

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+
 class SingleInstanceMetaClass(type):
     """
         https://www.pythonprogramming.in/singleton-class-using-metaclass-in-python.html
@@ -14,3 +18,12 @@ class SingleInstanceMetaClass(type):
         single_obj.__init__(*args, **kwargs)
         cls.__single_instance = single_obj
         return single_obj
+
+
+def get_env_variable(variable, json_dict, type_hint=None):
+    value = os.getenv(variable, None)
+    if not value:
+        value = json_dict.get(variable, None)
+    if value and type_hint:
+        value = type_hint(value)
+    return value
