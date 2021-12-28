0a73c024bf1d0bcaf5b32db15fd26e85cef93738
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2021-12-29 02:29:59 +0530

    add common_endpoints and tests folder

---
+++ app/api/api_v1/endpoints/common_endpoints.py
@@ -0,0 +1,30 @@
+from app.config.configurations import AppConfig
+import os
+from fastapi import Depends, HTTPException, status
+from fastapi.responses import HTMLResponse
+from fastapi_utils.inferring_router import InferringRouter
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+from app.models.request_response_models import CreateAccountRequest, CreateAccountResponse, LoginRequest, LoginResponse, UserProfileResponse
+from app.core import security
+
+router = InferringRouter(tags=["common"])
+template_dir_path = os.path.join(AppConfig.app_root_dir, "templates")
+templates = Jinja2Templates(directory=template_dir_path)
+
+
+@router.get("/")
+def get_home():
+    return "Hello there! Welcome to GetSeatGo!"
+
+
+# @router.get("/about", response_class=HTMLResponse)
+# def get_about(request: Request):
+#     # I need to create a session class, as layout.html is expecting a session.user_id variable (this variable is available in flask, without needing to pass it to the template)
+#     class session:
+#         def __init__(self, user_id=None):
+#             self.user_id = user_id
+
+#     return templates.TemplateResponse("about.html", {"request": request, "session": session()})
+    # return "GetSeatGo is a bus ticket booking system!"
