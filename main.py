259888a5559aabe58d6f4a5502758bc0ef3615d7
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2021-12-29 02:24:52 +0530

    make env variable name consistent to heroku (PORT)

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ from app.api.api_v1.endpoints import common_endpoints, users_endpoints
 from app.config.configurations import AppConfig
 from app.settings.app_settings import Settings
 
-title = AppConfig.title
-version = AppConfig.version
-description = AppConfig.description
+# print(Settings.print())
+# print(AppConfig.print())
+TITLE = AppConfig.TITLE
+VERSION = AppConfig.VERSION
+DESCRIPTION = AppConfig.DESCRIPTION
 
-host = AppConfig.host
-port = AppConfig.port
-reload = AppConfig.reload
-debug = AppConfig.debug
+HOST = AppConfig.HOST
+PORT = AppConfig.PORT
+RELOAD = AppConfig.RELOAD
+DEBUG = AppConfig.DEBUG
 
 tags_details = [
     {
@@ -27,15 +29,15 @@ tags_details = [
 ]
 
 origins = Settings.get_cors_list()
-app = FastAPI(title=title, version=version,
-              description=description, openapi_tags=tags_details)
+app = FastAPI(title=TITLE, version=VERSION,
+              description=DESCRIPTION, openapi_tags=tags_details)
 
 app.add_middleware(CORSMiddleware, allow_origins=origins,
                    allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(users_endpoints.router, prefix="")
-# app.include_router(common_endpoints.router, prefix="")
+app.include_router(common_endpoints.router, prefix="")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=host, port=port, reload=reload, debug=debug)
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=RELOAD, debug=DEBUG)
