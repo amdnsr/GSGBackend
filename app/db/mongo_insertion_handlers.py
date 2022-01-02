70b955c1b4d7696d96f8eefe1f96def20ba2d64f
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2022-01-03 03:43:23 +0530

    create handler for insertion of UserDetailsModel

---
+++ app/db/mongo_insertion_handlers.py
@@ -0,0 +1,31 @@
+from app.core.security import get_password_hash
+from app.db.mongo_handler import MongoHandlerBase
+from app.models.request_response_models import CreateAccountRequest
+from app.service.schema_models.user_details_model import user_details_connection_info, UserDetailsModel
+
+
+class UserDetailsInsertionHandler(MongoHandlerBase):
+    def __init__(self, mongo_connection_info) -> None:
+        super().__init__(mongo_connection_info)
+
+    def insert(self, user: CreateAccountRequest):
+        hashed_password = get_password_hash(user.password)
+        user_dict = user.dict()
+        user_dict["hashed_password"] = hashed_password
+
+        self.make_connection()
+
+        model_dict = {}
+        model_fields = UserDetailsModel._fields_ordered
+        for field in model_fields:
+            if field in user_dict:
+                model_dict[field] = user_dict[field]
+
+        user_model = UserDetailsModel(**model_dict)
+        db_obj = user_model.save()
+        self.close_connection()
+        return db_obj
+
+
+user_details_insertion_handler = UserDetailsInsertionHandler(
+    user_details_connection_info)
