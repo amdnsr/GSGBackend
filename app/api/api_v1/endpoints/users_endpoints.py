ca64673a68868472e10b58bcfad076443483b162
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2022-01-24 03:04:46 +0530

    mongoservice replaces handlers & add exceptions

--- app/api/api_v1/endpoints/users_endpoints.py
+++ app/api/api_v1/endpoints/users_endpoints.py
@@ -8,6 +8,8 @@ from app.core import security
 from app.utils.email_utils import generate_new_account_token, send_new_account_email, verify_new_account_token
 from app.db.mongo_insertion_handlers import user_details_insertion_handler
 from app.db.mongo_retrieval_handlers import user_details_retrieval_handler
+from app.service.mongo_service import MongoService
+from app.exceptions.exceptions import ExpiredSignatureError_Exception, InvalidJWTToken_Exception
 
 router = InferringRouter(tags=["users"])
 
@@ -23,17 +25,19 @@ def register(user: CreateAccountRequest):
 
     # Commenting it out for testing purpose
     # TODO remove the comments
-    existing_user = user_details_retrieval_handler.get_user_details_by_email(
-        email)
+    # existing_user = user_details_retrieval_handler.get_user_details_by_email(
+    #     email)
+    existing_user = MongoService.get_user_details_by_email(email)
     print("existing_user = ", existing_user)
     if existing_user:
         return "Sorry, an account already exists with this email!"
     first_name = user.first_name
-    db_obj = user_details_insertion_handler.insert(user)
+    # db_obj = user_details_insertion_handler.insert(user)
+    db_obj = MongoService.add_new_user(user)
     print("inserted user into db")
     token = generate_new_account_token(email)
     if not send_new_account_email(email, first_name, token):
-        user_details_insertion_handler.delete_account(email)
+        MongoService.delete_user(email)
         print("removed user from db")
         return f"Unable to send email to this address, please check your email again."
     return f"Check your mail for confirmation of account. Your user id is {db_obj.id}"
@@ -42,12 +46,19 @@ def register(user: CreateAccountRequest):
 @router.get("/confirm-account-creation")
 def confirm_account(token: str):
     # print(token)
-    email = verify_new_account_token(token)
+    try:
+        email = verify_new_account_token(token)
+    except ExpiredSignatureError_Exception:
+        return "The confirmation link has expired!"
+    except InvalidJWTToken_Exception:
+        return "The token is invalid!"
+        
     if email:
-        if user_details_insertion_handler.verify_account(email):
+        try:
+            MongoService.verify_user_email(email)
             return "Congratulations, you successfully activated your account!"
-        else:
-            status.HTTP_304_NOT_MODIFIED
+        except:
+            return status.HTTP_304_NOT_MODIFIED
     else:
         return status.HTTP_401_UNAUTHORIZED
 
@@ -55,11 +66,8 @@ def confirm_account(token: str):
 @router.post("/login", response_model=Union[LoginResponse, str])
 def login(login_request: LoginRequest):
     email = login_request.email
-    password = login_request.password
-    hashed_password = user_details_retrieval_handler.get_hashed_password(email)
-    if not hashed_password:
-        return "Sorry, wrong username/password!"
-    if not security.verify_password(password, hashed_password):
+    result = MongoService.validate_user_login(login_request)
+    if not result:
         return "Sorry, wrong username/password!"
 
     # hash the password and then check this in the users database
@@ -87,7 +95,8 @@ def login(login_request: LoginRequest):
 
 @router.get("/profile/me", response_model=Union[UserProfileResponse, str])
 def get_profile(email: str = Depends(security.auth_wrapper)):
-    user = user_details_retrieval_handler.get_user_details_by_email(email)
+    # user = user_details_retrieval_handler.get_user_details_by_email(email)
+    user = MongoService.get_user_details_by_email(email)
     if not user:
         return "Sorry your account does not exist!"
     return user
@@ -95,6 +104,6 @@ def get_profile(email: str = Depends(security.auth_wrapper)):
 
 @router.delete("/profile/delete", response_model=str)
 def delete_profile(email: str = Depends(security.auth_wrapper)):
-    if user_details_insertion_handler.delete_account(email):
+    if MongoService.delete_user(email):
         return "Successfully deleted the account!"
     return "Deletion unsuccessful!"
