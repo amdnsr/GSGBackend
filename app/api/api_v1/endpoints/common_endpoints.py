a001f1da46dc5be5d3b2336e6c97b9d9abbe7f82
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2022-01-26 00:20:12 +0530

    add about page

--- app/api/api_v1/endpoints/common_endpoints.py
+++ app/api/api_v1/endpoints/common_endpoints.py
@@ -8,6 +8,8 @@ from fastapi.templating import Jinja2Templates
 
 from app.models.request_response_models import CreateAccountRequest, CreateAccountResponse, LoginRequest, LoginResponse, UserProfileResponse
 from app.core import security
+from app.utils.helpers import FlaskJinjaSession
+
 
 router = InferringRouter(tags=["common"])
 template_dir_path = os.path.join(AppConfig.app_root_dir, "templates")
@@ -19,12 +21,9 @@ def get_home():
     return "Hello there! Welcome to GetSeatGo!"
 
 
-# @router.get("/about", response_class=HTMLResponse)
-# def get_about(request: Request):
-#     # I need to create a session class, as layout.html is expecting a session.user_id variable (this variable is available in flask, without needing to pass it to the template)
-#     class session:
-#         def __init__(self, user_id=None):
-#             self.user_id = user_id
+@router.get("/about", response_class=HTMLResponse)
+def get_about(request: Request):
+    # I need to create a session class, as layout.html is expecting a session.user_id variable (this variable is available in flask, without needing to pass it to the template)
 
-#     return templates.TemplateResponse("about.html", {"request": request, "session": session()})
-    # return "GetSeatGo is a bus ticket booking system!"
+    return templates.TemplateResponse("about.html", {"request": request, "session": FlaskJinjaSession()})
+    return "GetSeatGo is a bus ticket booking system!"
